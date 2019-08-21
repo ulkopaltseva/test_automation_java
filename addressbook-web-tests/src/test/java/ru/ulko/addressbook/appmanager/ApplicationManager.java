0ae7c6a8c6c73d302557a180604a5d97a66cb7e2
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-08-21 09:38:53 +0300

    перенос из AppManager ненужных там функций isAlert в класс HelperBase

--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/ApplicationManager.java
@@ -17,7 +17,6 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     public WebDriver driver;
     public String baseUrl;
-    public boolean acceptNextAlert = true;
     public StringBuffer verificationErrors = new StringBuffer();
 
     public void init() {
@@ -54,36 +53,5 @@ public class ApplicationManager {
         return contactHelper;
     }
 
-    public boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
 
-    public boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    public String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
 }
--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/HelperBase.java
@@ -1,13 +1,13 @@
 package ru.ulko.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 
 /**
  * Created by yulia on 21.08.2019.
  */
 public class HelperBase {
     protected WebDriver driver;
+    public boolean acceptNextAlert = true;
 
     public HelperBase(WebDriver driver) {
         this.driver = driver;
@@ -21,5 +21,38 @@ public class HelperBase {
 
     protected void click(By locator) {
         driver.findElement(locator).click();
+    }
+
+    public boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    public String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
     }
 }
