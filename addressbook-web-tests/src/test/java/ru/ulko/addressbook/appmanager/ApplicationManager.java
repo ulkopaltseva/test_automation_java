7b570698ce6203054501fedccf022de758b0b29f
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-08-21 18:17:17 +0300

    Optimize imports

--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,6 @@
 package ru.ulko.addressbook.appmanager;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
