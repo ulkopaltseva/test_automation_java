936fceec023c269a8971be35195d02f87a3911ab
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-08-23 09:30:16 +0300

    проверка, находимся ли мы уже на главной странице
    для метода gotoHomePage проверяем не находимся ли уже там (navigationHelper)
    еще в прошлы раз ошиблась, нам не нужно переходить на домашнюю страницу при создании контакта (правка в классе ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/NavigationHelper.java
@@ -22,6 +22,9 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home"));
     }
 }
--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,6 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initCreateContact();
     app.getContactHelper().fillContactData(new ContactData("First name", "Last Name", "8 sovet street, 31", "home phone", "monib phone", "work phone", "fax phone", "email", "email2", null, "test"), true);
     app.getContactHelper().submitCreationContact();
