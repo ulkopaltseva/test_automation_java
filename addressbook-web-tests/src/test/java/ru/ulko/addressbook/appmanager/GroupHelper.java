99c110290fc5c04b63c120070b9c9e13dce58a80
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-08-25 17:06:58 +0300

    получаем список групп getGroupList() c помощью списка ArrayList<GroupData>()
    элементы пока добавляем, опираясь на теги span с именем класса group
    driver.findElements(By.cssSelector("span.group"));

--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/appmanager/GroupHelper.java
@@ -2,7 +2,11 @@ package ru.ulko.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.ulko.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yulia on 20.08.2019.
@@ -77,5 +81,16 @@ public class GroupHelper extends HelperBase {
 
     public void selectGroup(int index) {
         getDriver().findElements(By.name("selected[]")).get(index).click();
+    }
+
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        List<WebElement> elements = getDriver().findElements(By.cssSelector("span.group"));
+        for (WebElement element: elements){
+            String name = element.getText();
+            GroupData group = new GroupData(name, null, null);
+            groups.add(group);
+        }
+        return groups;
     }
 }
--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/GroupCreationTests.java
@@ -5,15 +5,18 @@ import org.testng.annotations.*;
 
 import ru.ulko.addressbook.model.GroupData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupData("test", "header","footer"));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
 
 }
