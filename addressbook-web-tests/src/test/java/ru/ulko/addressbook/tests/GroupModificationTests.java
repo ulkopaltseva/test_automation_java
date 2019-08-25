9519e975c47deb7cce8d397954f1003cc18254b7
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-08-25 18:19:55 +0300

    добавлена проверка списков before и after в GroupModificationTests
    удаляем в списке before модифицированный эл-т и добавляем в него элемент group, которым мы заполняли новую группу

--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.ulko.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -18,9 +19,19 @@ public class GroupModificationTests extends TestBase {
             app.getGroupHelper().createGroup(new GroupData("test", "HEADER", "FOOTER"));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().modificateGroup(new GroupData("test", "HEADER", "FOOTER"), before.size() - 1);
+        GroupData group = new GroupData("test", "HEADER", "FOOTER");
+        app.getGroupHelper().modificateGroup(group, before.size() - 1);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        //удаляем элемент в списке before, который мы модифицировали
+        before.remove(before.size() - 1);
+
+        //добавляем новый объект, которым мы заменяли модифицируемый - group
+        before.add(group);
+
+        //сравниваем списки before и after, предворительно преобразовав их в множества - HashSet<Object>
+        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
 
     }
 
