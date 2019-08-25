de709f84d3cf4b69da20539ab2cbe069de646316
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-08-25 17:12:56 +0300

    использование метода getGroupList() в GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.ulko.addressbook.model.GroupData;
 
+import java.util.List;
+
 /**
  * Created by yulia on 21.08.2019.
  */
@@ -15,10 +17,10 @@ public class GroupModificationTests extends TestBase {
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test", "HEADER","FOOTER"));
         }
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().modificateGroup(new GroupData("test", "HEADER","FOOTER"), before - 1);
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().modificateGroup(new GroupData("test", "HEADER","FOOTER"), before.size() - 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
 
     }
 
