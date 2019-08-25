702a99f9f4eecb959cb75f351b5733271e614257
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-08-25 19:20:03 +0300

    коментарии в пакете model модуля addressbook

--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/model/GroupData.java
@@ -1,10 +1,12 @@
 package ru.ulko.addressbook.model;
 
+// форма заполнения группы при создании и модификации
 public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
 
+    // конструктор с инициализацией полей
     public GroupData(String name, String header, String footer) {
         this.name = name;
         this.header = header;
@@ -23,6 +25,7 @@ public class GroupData {
         return footer;
     }
 
+    // сгенерирован для сравнения объектов типа GroupData
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -34,11 +37,13 @@ public class GroupData {
 
     }
 
+    // сгенерирован для преобразования во множества (?)
     @Override
     public int hashCode() {
         return name != null ? name.hashCode() : 0;
     }
 
+    // сгенерирован, чтобы считывать тест полей в удобном виде
     @Override
     public String toString() {
         return "GroupData{" +
