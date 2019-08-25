be7f8e6062021df01c2ea727e2189c3b05bcdf17
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-08-25 19:20:08 +0300

    Create ContactData.java

---
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/model/ContactData.java
@@ -0,0 +1,75 @@
+package ru.ulko.addressbook.model;
+
+// форма заполнения контакта при создании и модификации
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String homePhone;
+    private final String mobilePhone;
+    private final String workPhone;
+    private final String faxPhone;
+    private final String email1;
+    private final String email2;
+    private final String email3;
+    private String group;
+
+    // конструктор с инициализацией полей контакта
+    public ContactData(String firstName, String lastName, String address, String homePhone, String mobilePhone, String workPhone, String faxPhone, String email1, String email2, String email3, String group) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.homePhone = homePhone;
+        this.mobilePhone = mobilePhone;
+        this.workPhone = workPhone;
+        this.faxPhone = faxPhone;
+        this.email1 = email1;
+        this.email2 = email2;
+        this.email3 = email3;
+        this.group = group;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    public String getFaxPhone() {
+        return faxPhone;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public String getEmail3() {
+        return email3;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+}
