8f93099f0e3f1d053d87ec194c0043dc646b7a4f
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-08-27 09:58:20 +0300

    удаление лишних реализаций максимального идентификатора в groupCreationTEst

--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/tests/GroupCreationTests.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 import ru.ulko.addressbook.model.GroupData;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,33 +18,16 @@ public class GroupCreationTests extends TestBase {
         // создать список before для хранения списка групп до создания новых
         List<GroupData> before = app.getGroupHelper().getGroupList();
 
-        // вычислить максимальное значение id  в списке before, чтобы задать правильные id для новых групп
-        int max = 0;
-        for (GroupData g: before){
-            if (g.getId() > max){
-                max = g.getId();
-            }
-        }
-
-        // вычислить максимальное значение id с помощью компаратора
-        Comparator<? super GroupData> ById = new Comparator<GroupData>() {
-            @Override
-            public int compare(GroupData o1, GroupData o2) {
-                return Integer.compare(o1.getId(), o2.getId());
-            }
-        };
-        int max1 = before.stream().max(ById).get().getId();
-
         // вычислить максимальное значение id с помощью лямбда-выражения
-        int max2 = before.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+        int max = before.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
 
 
         int i; // это для хранения количества созданных групп
         // в цикле создать несколько групп
         List<GroupData> differensBeforeAfter = new ArrayList<>(); //список групп, которые будут добавлены
         for (i = 1; i < 3; i++) {
-            max2++; // id каждой следующей группы
-            GroupData group = new GroupData(max2, "test_" + i, "header", "footer");
+            max++; // id каждой следующей группы
+            GroupData group = new GroupData(max, "test_" + i, "header", "footer");
             app.getGroupHelper().createGroup(group);
             differensBeforeAfter.add(group);
         }
@@ -62,7 +44,7 @@ public class GroupCreationTests extends TestBase {
 
 
         // добавить в список before созданные группы, добавленные в список differensBeforeAfter
-        for (GroupData d: differensBeforeAfter){
+        for (GroupData d : differensBeforeAfter) {
             before.add(d);
         }
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
