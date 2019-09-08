0e2c5ce0643641b7405dfa9e9a9f72cad5e55702
ulkopaltseva <ulkowhitehorse@yandex.ru>
2019-09-08 21:44:58 +0300

    GeneratorGroupsData обновлен с использованием библиотеки JCommander для описания параметров

--- addressbook-web-tests/src/test/java/ru/ulko/addressbook/generators/GeneratorGroupsData.java
+++ addressbook-web-tests/src/test/java/ru/ulko/addressbook/generators/GeneratorGroupsData.java
@@ -1,5 +1,8 @@
 package ru.ulko.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.ulko.addressbook.model.GroupData;
 
 import java.io.File;
@@ -14,14 +17,30 @@ import java.util.List;
  */
 public class GeneratorGroupsData {
 
+    @Parameter(names="-c", description = "Group count")
+    public int count;
+
+    @Parameter(names="-f", description = "Target file")
+    public String file;
+
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
-        List<GroupData> groups = generateGroups(count);
-        saveFile(groups, file);
+        GeneratorGroupsData generator = new GeneratorGroupsData();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
     }
 
-    private static void saveFile(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        saveFile(groups, new File(file));
+    }
+
+    private void saveFile(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for (GroupData group : groups) {
             writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader()
@@ -30,7 +49,7 @@ public class GeneratorGroupsData {
         writer.close();
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             groups.add(new GroupData().withName(String.format("name_%s", i))
